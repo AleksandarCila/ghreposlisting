bbee4fb263db3ce64b8404340054374c138685c4
Aleksandar <aca96@live.co.uk>
2023-01-25 09:59:31 +0100

    Updated page parameter loading from url

--- src/components/custom/RepoPanel/helpers/getInititalQueryParams.ts
+++ src/components/custom/RepoPanel/helpers/getInititalQueryParams.ts
@@ -1,15 +1,22 @@
 import { RepoPanelQueryState } from "./../types";
 
+const getSort = (search: URLSearchParams) => {
+  const searchSort = search.get("sort") || "stars";
+  if (searchSort === "stars" || searchSort === "forks") return searchSort;
+  return "stars";
+};
 
-const getSort=(search:URLSearchParams)=>{
-  const searchSort = search.get('sort') || 'stars';
-  if(searchSort === 'stars' || searchSort==='forks') return searchSort;
-  return 'stars';
-}
+const getPage = (search: URLSearchParams) => {
+  const page = parseInt(search.get("page") || "1");
+  if (page <= 0) return 1;
+  return page;
+};
 
-export const getInitialQueryParams = (search:URLSearchParams): RepoPanelQueryState => {
+export const getInitialQueryParams = (
+  search: URLSearchParams
+): RepoPanelQueryState => {
   return {
-    page: parseInt(search.get('page') || '1'),
+    page: getPage(search),
     per_page: 10,
     total: 0,
     sort: getSort(search),
